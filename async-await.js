58b324618598468d17c6d994ee89142dbd44a6ac
gustavoweb33 <lealgustavo33@gmail.com>
2019-11-04 21:44:37 -0600

    more examples

--- async-await.js
+++ async-await.js
@@ -6,7 +6,7 @@ const getDataPromise = ( num ) => new Promise( ( resolve, reject ) => {
 
 
 //a function with async always returns a promise with the data resolved and wraps non-promises in it
-async function processData()  {
+async function processData() {
     let data = await getDataPromise( 2 );
     //await literally makes JavaScript wait until the promise settles, and then go on with the result. 
     data = await getDataPromise( data );    //wont run until the first promise resolves or rejects
@@ -25,22 +25,22 @@ processData().then( ( data ) => {
 //these two functions return an error if the promise is rejected
 //they are both the same with different syntax
 async function anError() {
-    await Promise.reject(new Error("Whoops!"));
-  }
+    await Promise.reject( new Error( "Whoops!" ) );
+}
 
 async function anotherError() {
-    throw new Error('Some error occured');
+    throw new Error( 'Some error occured' );
 }
 
 //try catch error handling
 async function tryCatchExample() {
     try {
-        let response = await fetch(`some url`); 
+        let response = await fetch( `some url` );
         let user = await response.json();
     }
-    catch(error) {
+    catch ( error ) {
         // catches errors both in fetch and response.json
-        console.log(error);
+        console.log( error );
     }
 }
 
